a62d12249b4d0f5a8d020835ef9da5b20cf53a96
jared1291 <58518410+jared1291@users.noreply.github.com>
2019-12-09 08:44:36 -0500

    Update Yahtzee.java

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,3 +1,4 @@
+
 /*
  * File: Yahtzee.java
  * ------------------
@@ -9,13 +10,13 @@ import acm.program.*;
 import acm.util.*;
 
 public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
-	
+
 	private static final int turns = 3;
-	
+
 	public static void main(String[] args) {
 		new Yahtzee().start(args);
 	}
-	
+
 	public void run() {
 		IODialog dialog = getDialog();
 		nPlayers = dialog.readInt("Enter number of players");
@@ -24,88 +25,87 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 			playerNames[i - 1] = dialog.readLine("Enter name for player " + i);
 		}
 		display = new YahtzeeDisplay(getGCanvas(), playerNames);
+		initializeUsedCategories();
 		playGame();
 	}
-	
+
 	private void setCurrentPlayer() {
-		String currentPlayer = playerNames[playerCounter-1];
+		String currentPlayer = playerNames[nPlayers - 1];
 		display.printMessage(currentPlayer + "’s turn.  Roll the dice!");
-		}
-	
+	}
+
 	private void initializeUsedCategories() {
-		/*Creates a 2D array that stores the used and unused categories for each player 
-		 * so no one can re-use any category.  Also initializes the "total score" category for each player.*/
-	
-	usedCategories  = new int[nPlayers][N_CATEGORIES];
-	for(int i = 0; i < nPlayers; i++){
-		for(int y = 0; y < N_CATEGORIES; y++){
-			usedCategories[i][y] = -1;
+		/*
+		 * Creates a 2D array that stores the used and unused categories for each player
+		 * so no one can re-use any category. Also initializes the "total score"
+		 * category for each player.
+		 */
+
+		usedCategories = new int[nPlayers][N_CATEGORIES];
+		for (int i = 0; i < nPlayers; i++) {
+			for (int y = 0; y < N_CATEGORIES; y++) {
+				usedCategories[i][y] = -1;
+			}
+		}
+		playersTotalScore = new int[nPlayers];
+		for (int i = 0; i < nPlayers; i++) {
+			playersTotalScore[i] = 0;
 		}
 	}
-	playersTotalScore = new int[nPlayers];
-	for(int i = 0; i < nPlayers; i++){
-		playersTotalScore[i]=0;
-	}
-}
-	
 
 	private void playGame() {
-		
 
-	for(int i = 0; i < (nPlayers * N_SCORING_CATEGORIES); i++){
-			
-	setCurrentPlayer();
-//			firstRoll();
-//			secondAndThirdRoll();
+		// for(int i = 0; i < (nPlayers * N_SCORING_CATEGORIES); i++){
+
+		setCurrentPlayer();
+		firstRoll();
+		secondAndThirdRoll();
 //			selectACategory();
 //			evaluteDice(diceArray);
 //			checkCategory(category);
 //			display.updateScorecard(category, playerCounter, score);
 //			updateRunningTotal();
 //			setNextPlayer();
-//			
-}
-		
-		for (int i = 0; i < turns; i++) {
-			display.waitForPlayerToClickRoll(nPlayers);
-			makeDiceValues();
-			display.waitForPlayerToSelectDice();
-			makeDiceValuesLoop();
-			}
-		}
-	
-	
-	private void makeDiceValues() {
-		
-		for(int i = 0; i < N_DICE; i++) {
+//	
+
+	}
+
+	// }
+
+	private void firstRoll() {
+
+		display.waitForPlayerToClickRoll(nPlayers);
+		for (int i = 0; i < N_DICE; i++) {
 			diceVals[i] = rgen.nextInt(1, 6);
 		}
-		
+
 		display.displayDice(diceVals);
-		
+
 	}
-	
-	private void makeDiceValuesLoop() {
-//		if(display.isDieSelected(nPlayers)) {
-//			display.waitForPlayerToClickRoll(nPlayers);
-//			makeDiceValues();
-//	
-//		}
-		
-		
-		for (int i=0; i<N_DICE; i++) {
-			if (display.isDieSelected(i)) {
-			diceVals[i] = rgen.nextInt(1,6);
+
+	private void secondAndThirdRoll() {
+
+		for (int i = 0; i < 2; i++) {
+			display.waitForPlayerToSelectDice();
+			display.waitForPlayerToClickRoll(nPlayers);
+
+			for (int j = 0; j < N_DICE; j++) {
+				if (display.isDieSelected(j) == true) {
+					diceVals[j] = rgen.nextInt(1, 6);
+				}
+			}
+			display.displayDice(diceVals);
 			
 		}
-	}
-		display.displayDice(diceVals);
-		display.waitForPlayerToSelectCategory();
-		display.updateScorecard(N_SCORING_CATEGORIES, nPlayers, Score );
-	}
 		
+		for (int k = 0; k < diceVals.length; k++) {
+			Score += diceVals[k];
+		}
+		display.waitForPlayerToSelectCategory();
+		display.updateScorecard(N_SCORING_CATEGORIES, nPlayers, Score);
+	}
 
-/* Private instance variables */
+	/* Private instance variables */
 	private int nPlayers;
 	private String[] playerNames;
 	private YahtzeeDisplay display;
