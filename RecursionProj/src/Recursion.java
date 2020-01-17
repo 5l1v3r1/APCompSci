e9252cf08cdcc43bdecdba48ecbefaa4ad7d2f1e
vhansen21 <60004278+vhansen21@users.noreply.github.com>
2020-01-17 08:45:13 -0500

    Update Recursion.java

--- RecursionProj/src/Recursion.java
+++ RecursionProj/src/Recursion.java
@@ -4,63 +4,77 @@
  */
 
 
+
 import acm.program.*;
 
-public class Recursion extends ConsoleProgram{
-	
-	//serial ID
+public class Recursion extends ConsoleProgram {
+
+	// serial ID
 	private static final long serialVersionUID = 1L;
-	
+
 	String main = "";
-	int sum; 
-	String bin = ""; 
-	
-	//Problem 1 
+	int sum;
+	String bin = "";
+	int n;
+
+	// Problem 1
 	private String ReverseString(String str) {
-		
+
 		if (str.length() == 1) {
 			main += str.substring(0, 1);
-			return main; 
+			return main;
 		} else {
-			main += str.substring(str.length()-1, str.length());
-			str = str.substring(0, str.length()-1);
+			main += str.substring(str.length() - 1, str.length());
+			str = str.substring(0, str.length() - 1);
 			ReverseString(str);
 		}
-		return main; 
+		return main;
 	}
 
-	//Problem 2 
+	// Problem 2
 	private int SumOfDigits(int x) {
-		
-		
+
 		if (x < 10) {
 			sum += x;
 			return sum;
 		} else {
-			sum += x%10; 
-			x = x/10; 
+			sum += x % 10;
+			x = x / 10;
 			SumOfDigits(x);
 		}
 		return sum;
 	}
-	
-	//problem 3 
+
+	// problem 3
 	private String printInBinary(int number) {
 		if (number < 1) {
 			return bin;
 		} else {
-	
-			bin = number%2 + bin; 
-			number =number/2;
+
+			bin = number % 2 + bin;
+			number = number / 2;
 			printInBinary(number);
 		}
 		return bin;
 	}
 	
-	//testing problem outputs
+	//problem 4
+	private int GCD(int x, int y) {
+		if(x%y == 0) {
+			return y;
+		}else {
+			n = x%y;
+			GCD(n,y);
+		}
+		return n;
+	
+	}
+
+	// testing problem outputs
 	public void run() {
 		println(ReverseString("string"));
 		println(SumOfDigits(15704));
 		println(printInBinary(35));
+		println(GCD(6,3));
 	}
 }
